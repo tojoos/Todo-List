63ff8c79d89c1572d7b5ce3144b81593000935c6
ToJoos <joosx2000@gmail.com>
2023-07-24 12:27:59 +0200

    Show sql, add sample data import, fix typo in Priority

--- src/main/java/tojoos/todolist/pojo/Priority.java
+++ src/main/java/tojoos/todolist/pojo/Priority.java
@@ -1,7 +1,7 @@
 package tojoos.todolist.pojo;
 
 public enum Priority {
-  OW(1), MEDIUM(5),  HIGH(10);
+  LOW(1), MEDIUM(5),  HIGH(10);
 
   public final Integer value;
   Priority(Integer value) {
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO todoDB.task (id, description, priority, creation_time_stamp)
+VALUES ('217955c6-ad92-4506-8419-4d5a73406fce', 'Desc', 'HIGH', '2023-07-20 20:25:48.020579'),
+        ('49d4b7f3-cda7-44a7-915f-bdc66c70e657', 'Desc', 'HIGH', '2023-07-20 20:25:20.461483'),
+        ('85d3108b-040a-4ff6-96ff-ac5165d22303', 'Desc', 'LOW', '2023-07-20 20:25:37.593003'),
+        ('32d51ede-1ad1-449d-9025-85001bceb462', 'Desc', 'MEDIUM', '2023-07-20 20:26:46.325151'),
+        ('ac5c20bb-42fc-4fd9-934b-b3fd8fe0f9b6', 'Desc', 'HIGH', '2023-07-20 20:26:51.636714');
