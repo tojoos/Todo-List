350eed5e41d6b810996e38eac7b3f1f016dfb36c
ToJoos <joosx2000@gmail.com>
2023-07-24 14:05:09 +0200

    Add global exception handler with custom handlers

---
+++ src/main/java/tojoos/todolist/exception/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package tojoos.todolist.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(TaskNotFoundException.class)
+  public final ResponseEntity<Object> handleTaskNotFoundException(TaskNotFoundException ex, HttpServletRequest request) {
+    ApiError apiError = new ApiError(
+        LocalDateTime.now(),
+        request.getRequestURI(),
+        HttpStatus.NOT_FOUND,
+        ex.getErrorMessage()
+    );
+    log.error(ex.getErrorMessage());
+    return new ResponseEntity<>(apiError, apiError.status());
+  }
+
+  @ExceptionHandler({InvalidUUIDException.class, HttpMessageNotReadableException.class, IllegalArgumentException.class})
+  public final ResponseEntity<Object> handleInvalidUUIDException(RuntimeException ex, HttpServletRequest request) {
+    ApiError apiError = new ApiError(
+        LocalDateTime.now(),
+        request.getRequestURI(),
+        HttpStatus.BAD_REQUEST,
+        ex.getMessage()
+    );
+    log.error(ex.getMessage());
+    return new ResponseEntity<>(apiError, apiError.status());
+  }
+
+  @ExceptionHandler(Exception.class)
+  @ResponseBody
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public final ResponseEntity<Object> handleDefaultException(Exception ex, HttpServletRequest request) {
+    ApiError apiError = new ApiError(
+        LocalDateTime.now(),
+        request.getRequestURI(),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        ex.getMessage()
+    );
+    log.error(ex.getMessage());
+    return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
