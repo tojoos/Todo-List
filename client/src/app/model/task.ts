a2a9a98c0206bd325f7635b785219f43c1eaa670
ToJoos <joosx2000@gmail.com>
2023-07-26 19:17:07 +0200

    Add task entity

--- client/src/app/model/task.ts
+++ client/src/app/model/task.ts
@@ -2,7 +2,8 @@ import { Time } from "@angular/common";
 
 export interface Task {
   id: string,
+  title: string,
   description: string,
   priority: string,
-  creationTimeStamp: Time
+  creationTimeStamp: Date
 }
